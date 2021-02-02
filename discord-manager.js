556ccca32e9e883467cea623d0727ff7658c0220
Basar <74671944+Basarus@users.noreply.github.com>
2021-02-02 16:49:25 +0300

    Update discord-manager.js

--- discord-manager.js
+++ discord-manager.js
@@ -7,10 +7,12 @@ const require = createRequire(import.meta.url);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login('ODA2MDgwOTg4MDkzNjEyMDMy.YBkPaw.FAY3ByZp2E6sCy8TE7fR4l7P2Tc');
+client.login('TOKEN'); // Сюда указывать токен вашего бота
 
 console.log(chalk.cyanBright('Discord Connect!'));
 
+// config
+const channel_id; // CHANNEL ID
 
 let reports = [];
 
@@ -18,7 +20,7 @@ registerCmd('report', '', report);
 
 function report(player, args) {
 
-    client.channels.cache.get('806149305772539933').send({
+    client.channels.cache.get(channel_id).send({
         embed: {
             color: 3447003,
             author: {
@@ -44,14 +46,14 @@ client.on('message', message => {
     if (message.content[0] === '!') {
         let text = message.content.split(' ');
         let id = text[1];
-        if (isNaN(id)) return client.channels.cache.get('806149305772539933').send('Используй форму: !answer <id report> <answer>');
+        if (isNaN(id)) return client.channels.cache.get(channel_id).send('Используй форму: !answer <id report> <answer>');
         if (text[0] === '!answer' && reports[id] != undefined) {
             alt.Player.all.map(player => {
                 if (player.id == reports[id].id) {
                     text.splice(0, 2)
                     player.send(`{FF0000} Ответ на репорт: ${text.join(' ')}`)
                     reports.splice(id, 1)
-                } else client.channels.cache.get('806149305772539933').send('Такого репорта не существует!');
+                } else client.channels.cache.get(channel_id).send('Такого репорта не существует!');
             })
         }
     }
